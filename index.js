80e13647af8ed717818edba2cdf38f8a3ec7f794
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-03 17:02:11 +0530

    about get method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// console.log("hii")
+import express from "express"
+
+const app = express();
+const PORT =3000 || process.env.PORT
+const OBJ = {
+    msg : "WelCome! In Our Website"
+}
+
+app.get("/",(req,res)=>{
+    res.json({OBJ});
+})
+
+app.listen(PORT ,()=>{
+      console.log(`Server starts on the port ${PORT}`);
+})
