d5b3c7ed207a271237223e232288e5fb3569c8a6
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-04 22:31:11 +0530

    changes

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import express from "express"
 const app = express();
 const PORT =3000 || process.env.PORT
 
-const TASK = [{
-  task_name : "Wake Up at 4 AM",
-  task_description : "Follow EveryDay"
-},
-{
-  task_name : "Meditation",
-  task_description : "Train Your Brain With Good Habbit"
-}
-]
+const TASK = [
+        {
+            "task_name": "Meditation",
+            "task_description": "Train Your Brain With Good Habbit"
+        },
+        {
+            "task_name": "go to Jogging",
+            "task_description": "It help you to fitness your body"
+        }
+    
+];
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -34,8 +36,14 @@ app.get("/getTask",(req,res)=>{
 })
 app.post("/addTask",(req,res)=>{
    try {
-       console.log(req);
-      
+       let data=req.body;
+       if(!data){
+          res.json({
+             data : [],
+             msg : "Sorry there is not Data. Please Enter Data..."
+          })
+       }
+       TASK.push(data);
    } catch (error) {
     
    }
