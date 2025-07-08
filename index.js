6406d1f12054760793c6176232c57ddc9dc2a4bd
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-08 23:36:06 +0530

    changes added

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 import express from "express"
+import cors from "cors"
 
-const app = express();
-const PORT =3000 || process.env.PORT
+const app = express()
+const PORT = 3000 || process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ encoded: true }))
 
 const TASK = [
-        {
-            "task_name": "Meditation",
-            "task_description": "Train Your Brain With Good Habbit"
-        },
-        {
-            "task_name": "go to Jogging",
-            "task_description": "It help you to fitness your body"
-        }
-    
-];
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.get("/health",(req,res)=>{
-   res.send("Server become Healthy Now Thanks...")
-})
-app.get("/getTask",(req,res)=>{
-  try {
-     res.json({ data : TASK,
-        msg : "Have a Nice Day"
-     })
-  } catch (error) {
+    {
+        Taskname: "hello",
+        TaskDescription: "stay happy daily"
+    }
+]
+app.get("/health", (req, res) => {
     res.json({
-        data : [],
-        msg : "error"
+        data: [],
+        message: "server running healthy"
     })
-  }
-    
 })
-app.post("/addTask",(req,res)=>{
-   try {
-       let data=req.body;
-       if(!data){
-          res.json({
-             data : [],
-             msg : "Sorry there is not Data. Please Enter Data..."
-          })
-       }
-       TASK.push(data);
-   } catch (error) {
-    
-   }
+app.post("/addtask", (req, res) => {
+    try {
+        let data = req.body;
+        if (!data) {
+            res.json({
+                data: [],
+                message : "Data Not present"
+            })
+        }
+        TASK.push(data);
+    } catch (error) {
+        res.json({
+            data: [],
+            message: "something went wrong"
+        })
+    }
+})
+app.get("/gettask", (req, res) => {
+    try {
+        res.json({
+            data: TASK,
+            message: "message done"
+        })
+    } catch (error) {
+        res.json({
+            data: [],
+            message: error.message
+        })
+    }
 })
 
-app.listen(PORT ,()=>{
-      console.log(`Server starts on the port ${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`server runs on port : ${PORT}`);
 })
