3bd2f3141344dfccd3176a7f685d3fc7787e0298
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-14 21:10:07 +0530

    changes added

--- index.js
+++ index.js
@@ -1,128 +1,24 @@
 import express from "express"
 import cors from "cors"
-import SONG_INFO from "./DB.js"
 
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = 3000
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ encoded: true }))
-
-app.get("/songs", (req, res) => {
+app.get("/health" ,(req,res) => {
     try {
-        res.json(SONG_INFO);
+         res.json({
+            data : [],
+            message : "Server is running Healthy",
+            success : true
+         })
     } catch (error) {
-        res.json({
-            data: [],
-            message: "data not found"
-        })
+         res.json({
+            data : null,
+            message : error.message,
+            success : false
+         })
     }
 })
-app.post("/addSong", (req, res) => {
-    try {
-        let { name, song_url, poster } = req.body
-        if (!name || !song_url || !poster) {
-            res.json({
-                data: null,
-                message: error.message
-            })
-        }
-        let NewSong = {
-            id: SONG_INFO.length + 2,
-            name,
-            poster,
-            song_url,
-            like: false,
-            comment: []
-        }
-
-        SONG_INFO.push(NewSong)
-
-
-    } catch (error) {
-
-        res.json({
-            data: [],
-            message: error.message
-        })
-    }
-
-})
-app.post("/addComment/:id", (req, res) => {
-    try {
-        let { comment } = req.body;
-        let { id } = req.params
-
-        if (!id) {
-            res.json({
-                data: null,
-                message: "id is required"
-            })
-        }
-        if (!comment) {
-            res.json({
-                data: null,
-                message: "comment is required"
-            })
-        }
-
-        let foundSong = SONG_INFO.find((song) => song.id == id);
-        if (!foundSong) {
-            res.json({
-                data: [],
-                message: `Song Not Found For Id ${id}`
-            })
-        }
-        foundSong?.comment.unshift({
-            "name": "user",
-            comment
-        })
-
-    } catch (error) {
-        res.json({
-            data: null,
-            message: error.message
-        })
-    }
-})
-app.delete("/deletesong/:id", (req, res) => {
-    try {
-        let { id } = req.params
-        if (!id) {
-            res.json({
-                data: [],
-                message: "id must be required"
-            })
-        }
-        let FindSong = SONG_INFO.find((song) => song.id == id)
-        if (!FindSong == -1) {
-            res.json({
-                data: null,
-                message: error.message
-            })
-        }
-        let removeSong = SONG_INFO.splice(FindSong, 1)
-        return res.json({
-            data: removeSong,
-            message: " Song delete successfully "
-        })
-
-    } catch (error) {
-        res.json({
-            data: null,
-            message: error.message
-        })
-    }
-})
-
-app.get("/health", (req, res) => {
-    res.json({
-        data: [],
-        message: "server running healthy"
-    })
-})
-
-app.listen(PORT, () => {
-    console.log(`server runs on port : ${PORT}`);
+app.listen(PORT,()=>{
+     console.log(`server runs on the port ${PORT}`);
 })
